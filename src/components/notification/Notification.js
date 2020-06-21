46994b8244ceecf871bec9ad24c0b7041c7e7b78
ilyayudovin <ilyayudovin123@gmail.com>
2020-06-21 22:54:48 +0300

    fix eslint errors

--- src/components/notification/Notification.js
+++ src/components/notification/Notification.js
@@ -1,26 +1,46 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import React, { useState } from 'react';
 
-export const Notification = () => (
-  <div className="card">
-    <div className="text-center">
-      <button className="close" type="button" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-      <div className="card-header">Well Done!</div>
-      <div className="card-body">
-        <p className="card-text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
-        </p>
-        <a className="btn btn-primary m-1" href="#">
-          Settings
-        </a>
-        <a className="btn btn-primary m-1" href="#">
-          Continue
-        </a>
-      </div>
-    </div>
-  </div>
-);
+export const Notification = () => {
+  const [state, setState] = useState('false');
+
+  const closeNotification = () => {
+    setState('false');
+  };
+
+  const showNotification = () => {
+    setState('true');
+  };
+
+  return (
+    <>
+      {state === 'false' ? (
+        <button type="submit" className="btn btn-primary m-1" onClick={showNotification}>
+          Show Notification
+        </button>
+      ) : (
+        <div className="card">
+          <div className="text-center">
+            <button className="close" type="button" aria-label="Close" onClick={closeNotification}>
+              <span aria-hidden="true">&times;</span>
+            </button>
+            <div className="card-header">Well Done!</div>
+            <div className="card-body">
+              <p className="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a
+                ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat
+                a ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
+                erat a ante.
+              </p>
+              <button type="button" className="btn btn-primary m-1">
+                Settings
+              </button>
+              <button type="button" className="btn btn-primary m-1">
+                Continue
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
