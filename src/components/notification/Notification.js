afeb35a48c244e442aaadbfa0581a2aa668e17d0
ilyayudovin <ilyayudovin123@gmail.com>
2020-06-22 14:26:28 +0300

    feat/22: add isVisible state

--- src/components/notification/Notification.js
+++ src/components/notification/Notification.js
@@ -1,46 +1,44 @@
 import React, { useState } from 'react';
 
 export const Notification = () => {
-  const [state, setState] = useState('false');
+  const [state, setState] = useState({ isVisible: false });
 
   const closeNotification = () => {
-    setState('false');
+    setState({ isVisible: false });
   };
 
   const showNotification = () => {
-    setState('true');
+    setState({ isVisible: true });
   };
 
+  if (!state.isVisible) {
+    return (
+      <button type="submit" className="btn btn-primary m-1" onClick={showNotification}>
+        Show Notification
+      </button>
+    );
+  }
   return (
-    <>
-      {state === 'false' ? (
-        <button type="submit" className="btn btn-primary m-1" onClick={showNotification}>
-          Show Notification
+    <div className="card">
+      <div className="text-center">
+        <button className="close" type="button" aria-label="Close" onClick={closeNotification}>
+          <span aria-hidden="true">&times;</span>
         </button>
-      ) : (
-        <div className="card">
-          <div className="text-center">
-            <button className="close" type="button" aria-label="Close" onClick={closeNotification}>
-              <span aria-hidden="true">&times;</span>
-            </button>
-            <div className="card-header">Well Done!</div>
-            <div className="card-body">
-              <p className="card-text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a
-                ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat
-                a ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-                erat a ante.
-              </p>
-              <button type="button" className="btn btn-primary m-1">
-                Settings
-              </button>
-              <button type="button" className="btn btn-primary m-1">
-                Continue
-              </button>
-            </div>
-          </div>
+        <div className="card-header">Well Done!</div>
+        <div className="card-body">
+          <p className="card-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
+          </p>
+          <button type="button" className="btn btn-primary m-1">
+            Settings
+          </button>
+          <button type="button" className="btn btn-primary m-1">
+            Continue
+          </button>
         </div>
-      )}
-    </>
+      </div>
+    </div>
   );
 };
