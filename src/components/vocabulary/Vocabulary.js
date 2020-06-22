07e0c08e7dffecde80809bd40f168c2a79bfd7a1
ars6300 <ars6300@live.com>
2020-06-22 19:51:27 +0500

    feat/11: add api fetching, words rendering

--- src/components/vocabulary/Vocabulary.js
+++ src/components/vocabulary/Vocabulary.js
@@ -1,18 +1,63 @@
-import React from 'react';
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable react/no-danger */
+/* eslint-disable react/no-danger-with-children */
+import React, { useState, useEffect } from 'react';
 
-export const Vocabulary = () => (
-  <ul className="list-group list-group-flush">
-    <li className="list-group-item d-flex align-items-center">
-      <input className="mr-3" type="checkbox" checked="" />
-      <button className="btn" type="button">
-        <img className="mr-3" height="30" src="./assets/img/megaphone.svg" alt="Speak it" />
-      </button>
-      <div className="words-container">
-        <h5 className="text-primary mb-0">commitment</h5>
-        <p>обязательство</p>
-        <h6 className="mb-o">Do you know your commitments?</h6>
-        <p className="text-secondary mb-0">Давность: 11 часов назад | Повторений: 6</p>
-      </div>
-    </li>
-  </ul>
-);
+const apiUrl = 'https://afternoon-falls-25894.herokuapp.com/';
+const defaultQuery = 'words';
+
+export const Vocabulary = () => {
+  const [vocabularyState, setVocabularyState] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const wordsUrl = `${apiUrl}${defaultQuery}`;
+      try {
+        const res = await fetch(wordsUrl);
+        if (!res.ok) {
+          res.text().then(text => {
+            throw Error(text);
+          });
+        }
+        const data = await res.json();
+        setVocabularyState(data);
+      } catch (error) {
+        // ToDo: handle errors
+      }
+    }
+    fetchData();
+  }, []);
+  const playWord = e => {
+    e.preventDefault();
+    const link = vocabularyState.find(obj => obj.id === e.target.id).audio;
+    const audio = new Audio(`${apiUrl}${link}`);
+    audio.play();
+  };
+  function createMarkup(text) {
+    return { __html: text };
+  }
+  return (
+    <ul className="list-group list-group-flush">
+      {vocabularyState.map(word => {
+        return (
+          <li key={word.id} className="list-group-item d-flex align-items-center">
+            <button className="btn" type="button" onClick={e => playWord(e)}>
+              <img
+                className="mr-3"
+                height="30"
+                id={word.id}
+                src="./assets/img/megaphone.svg"
+                alt="Speak it"
+              />
+            </button>
+            <div className="words-container">
+              <h5 className="text-primary mb-0">{word.word}</h5>
+              <p>{word.wordTranslate}</p>
+              <p className="mb-o" dangerouslySetInnerHTML={createMarkup(word.textExample)}></p>
+              <p className="text-secondary mb-0">Давность: 11 часов назад | Повторений: 6</p>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
