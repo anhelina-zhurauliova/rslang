7f859c978ac207749557d1481954837bb763f7f0
Anhelina Zhurauliova <anhelinazhurauliova@gmail.com>
2020-06-23 10:27:07 +0300

    feat/9: add React Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.scss';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Header } from './components/header/Header';
-import { Notification } from './components/notification/Notification';
 import { Settings } from './components/settings/Settings';
 import { Card } from './components/card/Card';
 import { Authorization } from './components/authorization/Authorization';
@@ -9,14 +9,24 @@ import { Vocabulary } from './components/vocabulary/Vocabulary';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Notification />
-      <Settings />
-      <Card />
-      <Authorization />
-      <Vocabulary />
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/">
+          <Header />
+          <Authorization />
+        </Route>
+        <Route path="/settings">
+          <Settings />
+        </Route>
+        <Route path="/main">
+          <Header />
+          <Card />
+        </Route>
+        <Route path="/vocabulary">
+          <Vocabulary />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
