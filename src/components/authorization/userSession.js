30aa2be5a4796a3feacafdab44f1bb309db71dc4
kastrubait <60570588+kastrubait@users.noreply.github.com>
2020-06-27 20:18:18 +0300

    feat/8: add logout, checkSession

--- src/components/authorization/userSession.js
+++ src/components/authorization/userSession.js
@@ -2,9 +2,7 @@ const setSession = (name, value, options) => {
   if (options.expires instanceof Date) {
     options.expires = options.expires.toUTCString();
   }
-
   let updatedCookie = `${encodeURIComponent(name)}=${encodeURIComponent(value)}`;
-
   Object.keys(options).map(optionKey => {
     updatedCookie += `; ${optionKey}`;
     const optionValue = options[optionKey];
@@ -13,8 +11,40 @@ const setSession = (name, value, options) => {
     }
     return updatedCookie;
   });
-
   document.cookie = updatedCookie;
 };
 
-export { setSession };
+const getSession = cname => {
+  const name = `${cname}=`;
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return '';
+};
+
+const checkSession = () => {
+  const { isLoggedIn } = getSession('authUser');
+  if (isLoggedIn && isLoggedIn != null) {
+    // Logged in //do nothing
+  } else {
+    window.location.href = '';
+  }
+};
+
+const logoutUser = () => {
+  const authState = {
+    isLoggedIn: false,
+    user: {},
+  };
+  setSession('authState', JSON.stringify(authState), { secure: true, expires: 0 });
+};
+
+export { setSession, getSession, checkSession, logoutUser };
