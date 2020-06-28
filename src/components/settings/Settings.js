86d8c4d7b41480a7361bad143add3631f85f6896
Artyom Yermak <artyom.rus.ct100@mail.ru>
2020-06-28 21:01:23 +0300

    feat/15: fix bugs

--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -1,237 +1,158 @@
+/* eslint-disable no-console */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
+// TODO: Take these from cookies
 const auth = {
   message: 'Authenticated',
   token: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlZWJhYjU4OThmZmJmMDAxNzQ1ODFlOSIsImlhdCI6
-  MTU5MjkzNjk4MywiZXhwIjoxNTkyOTUxMzgzfQ.130z1Euhr0B0rI4AFdQe3RgtJI46uihVvQnT2OAqqAU`,
+    MTU5MjkzNjk4MywiZXhwIjoxNTkyOTUxMzgzfQ.130z1Euhr0B0rI4AFdQe3RgtJI46uihVvQnT2OAqqAU`,
   userId: '5eebab5898ffbf00174581e9',
 };
 
-const urlSettings = `https://afternoon-falls-25894.herokuapp.com/users/${auth.userId}/settings`;
-export class Settings extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      limitWords: '',
-      limitCards: '',
-      translate: '',
-      transcription: '',
-      image: '',
-      sentenceUnderstanding: '',
-      sentenceExample: '',
-      deleteWords: '',
-      difficultWords: '',
-      complexity: '',
-    };
+export const Settings = () => {
+  const [settings, setSettings] = useState(null);
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClickSettings = this.handleClickSettings.bind(this);
-    this.handleClickSaveSettings = this.handleClickSaveSettings.bind(this);
+  // TODO: Get data here
+  useEffect(() => {
+    fetch('')
+      .then(response => setSettings(response))
+      .catch(error =>
+        // TODO: Show some notification
+        console.log(error),
+      );
+  }, []);
+
+  if (!settings) {
+    return <h2>Loading...</h2>;
   }
 
-  handleChange = event => {
-    this.destruction();
-    const {
-      target: { name, value },
-    } = event;
-    this.setState({ [name]: value });
-  };
+  return (
+    <div>
+      <h1>Anywhere in your app!</h1>
+      <Formik
+        initialValues={settings}
+        validate={values => {
+          const errors = {};
+          if (!values.wordsLimit) {
+            errors.wordsLimit = 'Required';
+          }
+          if (!values.cardsLimit) {
+            errors.cardsLimit = 'Required';
+          }
+          if (values.wordsLimit < 10 || values.wordsLimit > 200) {
+            errors.wordsLimit = 'Invalid words limit';
+          }
+          if (values.cardsLimit < 10 || values.cardsLimit > 200) {
+            errors.cardsLimit = 'Invalid cards limit';
+          }
+          if (values.wordsLimit > values.cardsLimit) {
+            errors.cardsLimit = 'Cards limit should be more than words limit';
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          console.log('values');
+          console.log(values);
+          setSubmitting(true);
 
-  async handleClickSettings(event) {
-    const { limitWords, ...settings } = this.state;
-    JSON.stringify(settings);
-    await fetch(urlSettings, {
-      method: 'GET',
-      headers: {
-        Authorization: `${auth.token}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        limitWords,
-        settings,
-      }),
-    }).then(response => response.json());
-    // .catch(error => {
-    //   console.error('Error:', error);
-    // });
-    event.preventDefault();
-  }
-
-  async handleClickSaveSettings(event) {
-    await fetch(this.urlSettings, {
-      method: 'PUT',
-      headers: {
-        Authorization: `${auth.token}`,
-        'Content-Type': 'application/json',
-      },
-    }).then(response => response.json());
-    // .catch(error => {
-    //   console.error('Error:', error);
-    // });
-    event.preventDefault();
-  }
-
-  isValid() {
-    const { limitWords, limitCards } = this.state;
-    if (limitWords < 10 || limitWords > 200) {
-      return false;
-    }
-    if (limitCards < 10 || limitCards > 500) {
-      return false;
-    }
-    if (limitWords > limitCards) {
-      return false;
-    }
-    return true;
-  }
-
-  render() {
-    const {
-      limitWords,
-      limitCards,
-      translate,
-      transcription,
-      image,
-      sentenceUnderstanding,
-      sentenceExample,
-      deleteWords,
-      difficultWords,
-      complexity,
-    } = this.state;
-    return (
-      <div className="container">
-        <button className="btn btn-secondary" type="button" onClick={this.handleClickSettings}>
-          Settings
-        </button>
-        <button
-          disabled={this.isValid()}
-          className="btn btn-secondary"
-          type="button"
-          onClick={this.handleClickSaveSettings}
-          errorMessage={this.isValid() ? '' : 'This field is required'}
-        >
-          Save Settings
-        </button>
-        <div className="input-group input-group-sm mb-3">
-          <div className="input-group-prepend">
-            <span className="input-group-text ">Daily limit of words</span>
+          // TODO: Put data here
+          fetch('', {
+            method: 'PUT',
+            headers: {
+              Authorization: `${auth.token}`,
+              'Content-Type': 'application/json',
+            },
+          })
+            .then(response => response.json())
+            .catch(error => {
+              // TODO: Handle errors with some notifications or toasters
+              console.error('Error:', error);
+            })
+            .finally(() => setSubmitting(false));
+        }}
+      >
+        {({ isSubmitting }) => (
+          <div className="container">
+            <Form>
+              <div className="input-group input-group-sm mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text ">Daily limit of words</span>
+                </div>
+                <Field className="form-control has-error" type="number" name="wordsLimit" />
+              </div>
+              <ErrorMessage className="is-invalid" name="wordsLimit" component="div" />
+              <div className="input-group input-group-sm mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text">Daily limit of cards</span>
+                </div>
+                <Field className="form-control has-error" type="number" name="cardsLimit" />
+              </div>
+              <ErrorMessage className="is-invalid" name="cardsLimit" component="div" />
+              <div className="form-group form-check">
+                <Field className="form-check-input" type="checkbox" name="translate" />
+                <label className="form-check-label" htmlFor="exampleCheck1">
+                  Translate
+                </label>
+              </div>
+              <ErrorMessage className="is-invalid" name="translate" component="div" />
+              <div className="form-group form-check">
+                <Field className="form-check-input" type="checkbox" name="transcription" />
+                <label className="form-check-label" htmlFor="exampleCheck1">
+                  Transcription
+                </label>
+              </div>
+              <ErrorMessage className="is-invalid" name="transcription" component="div" />
+              <div className="form-group form-check">
+                <Field className="form-check-input" type="checkbox" name="image" />
+                <label className="form-check-label" htmlFor="exampleCheck1">
+                  Image
+                </label>
+              </div>
+              <ErrorMessage className="is-invalid" name="image" component="div" />
+              <div className="form-group form-check">
+                <Field className="form-check-input" type="checkbox" name="sentenceUnderstanding" />
+                <label className="form-check-label" htmlFor="exampleCheck1">
+                  Sentence for best understanding
+                </label>
+              </div>
+              <ErrorMessage className="is-invalid" name="sentenceUnderstanding" component="div" />
+              <div className="form-group form-check">
+                <Field className="form-check-input" type="checkbox" name="sentenceExample" />
+                <label className="form-check-label" htmlFor="exampleCheck1">
+                  Sentence example of usage
+                </label>
+              </div>
+              <ErrorMessage className="is-invalid" name="sentenceExample" component="div" />
+              <div className="form-group form-check">
+                <Field className="form-check-input" type="checkbox" name="deleteWords" />
+                <label className="form-check-label" htmlFor="exampleCheck1">
+                  Delete
+                </label>
+              </div>
+              <ErrorMessage className="is-invalid" name="deleteWords" component="div" />
+              <div className="form-group form-check">
+                <Field className="form-check-input" type="checkbox" name="difficultWords" />
+                <label className="form-check-label" htmlFor="exampleCheck1">
+                  Difficult Words
+                </label>
+              </div>
+              <ErrorMessage className="is-invalid" name="difficultWords" component="div" />
+              <div className="form-group form-check">
+                <Field className="form-check-input" type="checkbox" name="complexity" />
+                <label className="form-check-label" htmlFor="exampleCheck1">
+                  Complexity
+                </label>
+              </div>
+              <ErrorMessage className="is-invalid" name="complexity" component="div" />
+              <button type="submit" disabled={isSubmitting}>
+                Save
+              </button>
+            </Form>
           </div>
-          <input
-            className="form-control has-error"
-            type="number"
-            min="10"
-            max="100"
-            aria-label="Sizing example input"
-            aria-describedby="inputGroup-sizing-sm"
-            onChange={this.handleChange}
-            value={limitWords}
-          />
-        </div>
-        <div className="input-group input-group-sm mb-3">
-          <div className="input-group-prepend">
-            <span className="input-group-text">Daily limit of cards</span>
-          </div>
-          <input
-            className="form-control has-error"
-            type="number"
-            min="10"
-            max="100"
-            aria-label="Sizing example input"
-            aria-describedby="inputGroup-sizing-sm"
-            onChange={this.handleChange}
-            value={limitCards}
-          />
-        </div>
-        <div className="form-group form-check">
-          <input
-            className="form-check-input"
-            type="checkbox"
-            value={translate}
-            onChange={this.handleChange}
-          />
-          <label className="form-check-label" htmlFor="exampleCheck1">
-            Translate
-          </label>
-        </div>
-        <div className="form-group form-check">
-          <input
-            className="form-check-input"
-            type="checkbox"
-            value={transcription}
-            onChange={this.handleChange}
-          />
-          <label className="form-check-label" htmlFor="exampleCheck1">
-            Transcription
-          </label>
-        </div>
-        <div className="form-group form-check">
-          <input
-            className="form-check-input"
-            type="checkbox"
-            value={image}
-            onChange={this.handleChange}
-          />
-          <label className="form-check-label" htmlFor="exampleCheck1">
-            Image
-          </label>
-        </div>
-        <div className="form-group form-check">
-          <input
-            className="form-check-input"
-            type="checkbox"
-            value={sentenceUnderstanding}
-            onChange={this.handleChange}
-          />
-          <label className="form-check-label" htmlFor="exampleCheck1">
-            Sentence for best understanding
-          </label>
-        </div>
-        <div className="form-group form-check">
-          <input
-            className="form-check-input"
-            type="checkbox"
-            value={sentenceExample}
-            onChange={this.handleChange}
-          />
-          <label className="form-check-label" htmlFor="exampleCheck1">
-            Sentence example of usage
-          </label>
-        </div>
-        <div className="form-group form-check">
-          <input
-            className="form-check-input"
-            type="checkbox"
-            value={deleteWords}
-            onChange={this.handleChange}
-          />
-          <label className="form-check-label" htmlFor="exampleCheck1">
-            Delete
-          </label>
-        </div>
-        <div className="form-group form-check">
-          <input
-            className="form-check-input"
-            type="checkbox"
-            value={difficultWords}
-            onChange={this.handleChange}
-          />
-          <label className="form-check-label" htmlFor="exampleCheck1">
-            Difficult Words
-          </label>
-        </div>
-        <div className="form-group form-check">
-          <input
-            className="form-check-input"
-            type="checkbox"
-            value={complexity}
-            onChange={this.handleChange}
-          />
-          <label className="form-check-label" htmlFor="exampleCheck1">
-            Complexity
-          </label>
-        </div>
-      </div>
-    );
-  }
-}
+        )}
+      </Formik>
+    </div>
+  );
+};
