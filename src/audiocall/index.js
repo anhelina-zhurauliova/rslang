0d3b8033147107888b7e1e5b58163d2c0883031c
ars6300 <ars6300@live.com>
2020-07-04 17:41:33 +0500

    feat/29: trying to route start screen

--- src/audiocall/index.js
+++ src/audiocall/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import { StartScreen } from './StartScreen';
+/* eslint-disable max-len */
+import React, { useState } from 'react';
+import { Game } from './Game';
 import './audiocall.scss';
 
 export const AudioCall = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [screen, setScreen] = useState();
   return (
     <div className="audiocall container d-flex justify-content-center">
-      <StartScreen />
+      <div className="audiocall__start-container col-lg-8 col-md-10 container d-flex justify-content-center align-items-center flex-column">
+        <h1 className="start-header">аудиовызов</h1>
+        <p className="start-text">Улучшает восприятие английской речи на слух</p>
+        <button className="btn-next btn" type="button" onClick={setScreen(<Game />)}>
+          Начать
+        </button>
+      </div>
     </div>
   );
 };
