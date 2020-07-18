cd35d987c590198dc00abc52835adcc39f876ecb
Anhelina Zhurauliova <anhelinazhurauliova@gmail.com>
2020-07-18 21:59:40 +0300

    fix: fix styles

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,9 @@
-import React, { useEffect } from 'react';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
+import classNames from 'classnames';
 import { useAppContext } from '../../libs/contextLib';
 
 import './header.scss';
@@ -9,6 +12,7 @@ export const Header = () => {
   const { isAuthenticated, userHasAuthenticated } = useAppContext();
   // eslint-disable-next-line no-unused-vars
   const [cookies, setCookies] = useCookies(['authState']);
+  const [isClicked, setIsCliked] = useState(false);
   const history = useHistory();
 
   function handleLogout() {
@@ -18,12 +22,55 @@ export const Header = () => {
   }
   useEffect(() => {}, []);
 
+  const onMenuClickHandler = () => {
+    if (isClicked) {
+      setIsCliked(false);
+    } else {
+      setIsCliked(true);
+    }
+  };
+  const navContainerClass = classNames({
+    navigation__container: true,
+    hidden__navigation__container: !isClicked,
+  });
+  const burgerClass = classNames({
+    burger__container: true,
+    pushed: isClicked,
+  });
+  // const stickClass1 = classNames({
+  //   stick: true,
+  //   'stick-1': true,
+  //   'close ': isClicked,
+  //   open: isClicked,
+  // });
+  // const stickClass2 = classNames({
+  //   stick: true,
+  //   'stick-2': true,
+  //   close: isClicked,
+  //   open: !isClicked,
+  // });
+  // const stickClass3 = classNames({
+  //   stick: true,
+  //   'stick-3': true,
+  //   close: !isClicked,
+  //   o: isClicked,
+  // });
+
+  //  const studiedWordClass = classNames({
+  //    'studied-word__container': true,
+  //    hidden: shouldShowStudiedWord === false || !shouldShowWordOnFocus,
+  //  });
+  //  const wordToShow = classNames({
+  //    'studied-word-to-show': true,
+  //    hidden: shouldShowInput,
+  //  });
+
   return (
     <div className="wrapper__header">
       <header className="header__container">
         <span className="logo" />
         <h1 className="text__logo">RocketEnglish</h1>
-        <nav className="navigation__container">
+        <nav className={navContainerClass}>
           <ul className="navigation-links__container">
             {isAuthenticated ? (
               <>
@@ -74,6 +121,13 @@ export const Header = () => {
             )}
           </ul>
         </nav>
+        <div className={burgerClass}>
+          <div className="toggle-icon" onClick={onMenuClickHandler}>
+            <span className="bar" />
+            <span className="bar" />
+            <span className="bar" />
+          </div>
+        </div>
       </header>
     </div>
   );
